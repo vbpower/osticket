376c3b97325049e7f18c7ca49cedaa72eb159c05
vbpower <86586404+vbpower@users.noreply.github.com>
2021-06-29 11:43:21 +0900

    Update ost-config.php

--- include/ost-config.php
+++ include/ost-config.php
@@ -38,10 +38,10 @@ define('ADMIN_EMAIL','demouser@opsgility.com');
 # Database Options
 # ---------------------------------------------------
 # Mysql Login info
-define('DBTYPE','mysql');
-define('DBHOST','yourservername.mysql.database.azure.com');
+define('DBTYPE','user23MySQL');
+define('DBHOST','user23osticketsrv01.mysql.database.azure.com');
 define('DBNAME','osticket');
-define('DBUSER','demouser@yourservername');
+define('DBUSER','demouser@user23osticketsrv01');
 define('DBPASS','demo@pass123');
 
 # Table prefix
